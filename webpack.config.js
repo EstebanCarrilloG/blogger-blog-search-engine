1a8e82ce0ecb17fd324bf7594ae88a4b8e421742
EstebanCarrilloG <estexav0996@gmail.com>
2024-12-15 18:34:08 -0500

    jQuery enabled in development mode only

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
-const htmlWebpackPluging = require('html-webpack-plugin');
-const { template } = require('webpack');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const isProduction = true; // Modo de ejecución
 
 module.exports = {
-
-
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-       
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-
-        }),
-        new htmlWebpackPluging({
-            filename:'index.html',
-            template: './src/index.html'
-        })
+  mode: isProduction ? "production" : "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-}
+  },
+  externals: isProduction
+    ? {
+        jquery: "jQuery", // Solo excluye jQuery en producción
+      }
+    : {},
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+};
