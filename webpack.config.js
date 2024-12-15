ab493e68ac64b43ac0fa67971466f4ce78a79901
EstebanCarrilloG <estexav0996@gmail.com>
2024-12-15 17:58:19 -0500

    preparing for 2024 version

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const htmlWebpackPluging = require('html-webpack-plugin');
-const { template } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-
-
-    mode: 'production',
-    entry: './src/index.js',
+    mode: 'production', // Modo de producción
+    entry: './src/index.js', // Archivo de entrada
     output: {
-        path: __dirname + '/dist',
-        filename: 'index.js'
+        path: __dirname + '/dist', // Carpeta de salida
+        filename: 'index.js', // Nombre del archivo generado
     },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.css$/i, // Procesar archivos CSS
+                use: ['style-loader', 'css-loader'],
             },
         ],
-       
+    },
+    externals: {
+        jquery: 'jQuery', // No incluir jQuery en el bundle, tomarlo como global o externo
     },
     plugins: [
+        // Permitir el uso de jQuery como $ y jQuery
         new webpack.ProvidePlugin({
             $: 'jquery',
-            jQuery: 'jquery'
-
+            jQuery: 'jquery',
         }),
-        new htmlWebpackPluging({
-            filename:'index.html',
-            template: './src/index.html'
-        })
+        // Plugin para inyectar tu bundle en el archivo HTML
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
     ],
-}
+};
